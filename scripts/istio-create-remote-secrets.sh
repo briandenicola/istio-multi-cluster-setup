23ddcf192149eb2c695d9757d0052ac1a07614ff
briandenicola <brian@briandenicola.com>
2022-05-11 15:26:21 -0500

    Broke into multiple steps

--- scripts/istio-create-remote-secrets.sh
+++ scripts/istio-create-remote-secrets.sh
@@ -2,11 +2,11 @@
 
 az login --identity
 
-az account set -s ${CENTRAL_SUBSCRIPTION_ID}
-az aks get-credentials -g ${CE_CLUSTER_RG} -n ${CE_CLUSTER_NAME} --overwrite-existing
+az account set -s ${PRIMARY_SUBSCRIPTION_ID}
+az aks get-credentials -g ${PRIMARY_CLUSTER_RG} -n ${PRIMARY_CLUSTER_NAME} --overwrite-existing
 
-az account set -s ${SOUTHCENTRAL_SUBSCRIPTION_ID}
-az aks get-credentials -g ${SC_CLUSTER_RG} -n ${SC_CLUSTER_NAME} --overwrite-existing
+az account set -s ${SECONDARY_SUBSCRIPTION_ID}
+az aks get-credentials -g ${SECONDARY_CLUSTER_RG} -n ${SECONDARY_CLUSTER_NAME} --overwrite-existing
 
-istioctl x create-remote-secret --context="${CE_CLUSTER_NAME}" --name="${CE_CLUSTER_NAME}"| kubectl apply -f - --context="${SC_CLUSTER_NAME}"
-istioctl x create-remote-secret --context="${SC_CLUSTER_NAME}" --name="${SC_CLUSTER_NAME}"| kubectl apply -f - --context="${CE_CLUSTER_NAME}"
+istioctl x create-remote-secret --context="${PRIMARY_CLUSTER_NAME}" --name="${PRIMARY_CLUSTER_NAME}"| kubectl apply -f - --context="${SECONDARY_CLUSTER_NAME}"
+istioctl x create-remote-secret --context="${SECONDARY_CLUSTER_NAME}" --name="${SECONDARY_CLUSTER_NAME}"| kubectl apply -f - --context="${PRIMARY_CLUSTER_NAME}"
--- scripts/istio-create-trusted-ca.sh
+++ scripts/istio-create-trusted-ca.sh
@@ -2,20 +2,11 @@
 
 az login --identity
 
-az account set -s ${CENTRAL_SUBSCRIPTION_ID}
-az aks get-credentials -g ${CE_CLUSTER_RG} -n ${CE_CLUSTER_NAME} --overwrite-existing
+az account set -s ${SUBSCRIPTION_ID}
+az aks get-credentials -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --overwrite-existing
 
-az account set -s ${SOUTHCENTRAL_SUBSCRIPTION_ID}
-az aks get-credentials -g ${SC_CLUSTER_RG} -n ${SC_CLUSTER_NAME} --overwrite-existing
-
-kubectl --context="${CE_CLUSTER_NAME}" create secret generic cacerts -n istio-system \
- --from-literal=ca-cert.pem=${CE_CLUSTER_CA_CERT} \
- --from-literal=ca-key.pem=${CE_CLUSTER_CA_KEY} \
- --from-literal=root-cert.pem=${CE_CLUSTER_ROOT_CA_CERT} \
- --from-literal=cert-chain.pem=${CE_CLUSTER_CERT_CHAIN} \
-
-kubectl --context="${SC_CLUSTER_NAME}" create secret generic cacerts -n istio-system \
- --from-literal=ca-cert.pem=${SC_CLUSTER_CA_CERT} \
- --from-literal=ca-key.pem=${SC_CLUSTER_CA_KEY} \
- --from-literal=root-cert.pem=${SC_CLUSTER_ROOT_CA_CERT} \
- --from-literal=cert-chain.pem=${SC_CLUSTER_CERT_CHAIN} \
+kubectl --context="${CLUSTER_NAME}" create secret generic cacerts -n istio-system \
+ --from-literal=ca-cert.pem=${CLUSTER_CA_CERT} \
+ --from-literal=ca-key.pem=${CLUSTER_CA_KEY} \
+ --from-literal=root-cert.pem=${CLUSTER_ROOT_CA_CERT} \
+ --from-literal=cert-chain.pem=${CLUSTER_CERT_CHAIN} \
