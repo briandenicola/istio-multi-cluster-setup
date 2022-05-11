6789ae4e5a0f42064828105bfadd1ff3e4ad0822
briandenicola <brian@briandenicola.com>
2022-05-11 14:18:19 -0500

    Pipeline updates

--- scripts/aks-pod-identity-creation.sh
+++ scripts/aks-pod-identity-creation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 az account set -s ${SUBSCRIPTION_ID}
+az aks get-credentials -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --overwrite-existing
 
 RESOURCEID=`az identity show --name ${IDENTITY_NAME} --resource-group ${IDENTITY_RG} --query id -o tsv`
 az aks pod-identity add --resource-group ${CLUSTER_RG} --cluster-name ${CLUSTER_NAME} --namespace ${NAMESPACE} --name ${IDENTITY_NAME} --identity-resource-id ${RESOURCEID}
