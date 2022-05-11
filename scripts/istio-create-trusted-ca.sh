034dcabfa3cb9d4f2dbda65d831f3cc3a9f8d413
briandenicola <brian@briandenicola.com>
2022-05-11 15:26:58 -0500

    Fixed bad line break

--- scripts/istio-create-trusted-ca.sh
+++ scripts/istio-create-trusted-ca.sh
@@ -9,4 +9,4 @@ kubectl --context="${CLUSTER_NAME}" create secret generic cacerts -n istio-syste
  --from-literal=ca-cert.pem=${CLUSTER_CA_CERT} \
  --from-literal=ca-key.pem=${CLUSTER_CA_KEY} \
  --from-literal=root-cert.pem=${CLUSTER_ROOT_CA_CERT} \
- --from-literal=cert-chain.pem=${CLUSTER_CERT_CHAIN} \
+ --from-literal=cert-chain.pem=${CLUSTER_CERT_CHAIN} 
