7d467d4be2a549e7feb5511bb3b78a6218026579
briandenicola <brian@briandenicola.com>
2022-05-11 14:27:35 -0500

    Formatting

--- scripts/aks-flux-configuration.sh
+++ scripts/aks-flux-configuration.sh
@@ -6,6 +6,4 @@ az aks get-credentials -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --overwrite-existing
 kubelogin convert-kubeconfig -l msi
 
 kubectl create ns flux-system || true
-
-
 flux bootstrap github --owner=${GITHUB_ACCOUNT} --repository=${GITHUB_REPO} --path=${CLUSTER_BOOTSTRAP_PATH} --branch=${REPO_BRANCH}  --personal=true --private=false
