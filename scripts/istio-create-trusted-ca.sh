38fb64608d3c8a27954b971389f37b7f85f2b700
Brian Denicola <brian@briandenicola.com>
2022-05-13 12:18:48 -0500

    Namespace required for secret

--- scripts/istio-create-trusted-ca.sh
+++ scripts/istio-create-trusted-ca.sh
@@ -5,6 +5,7 @@ az login --identity
 az account set -s ${SUBSCRIPTION_ID}
 az aks get-credentials -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --overwrite-existing
 
+kubectl create ns istio-system
 kubectl --context="${CLUSTER_NAME}" create secret generic cacerts -n istio-system \
  --from-literal=ca-cert.pem=${CLUSTER_CA_CERT} \
  --from-literal=ca-key.pem=${CLUSTER_CA_KEY} \
