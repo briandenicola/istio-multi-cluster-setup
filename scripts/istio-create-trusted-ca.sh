a2f086827f200623af4a3ec8640756668038ebdb
Brian Denicola <brian@briandenicola.com>
2022-05-13 12:38:42 -0500

    Moved to more managable method

--- scripts/istio-create-trusted-ca.sh
+++ scripts/istio-create-trusted-ca.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+export NAMESPACE=istio-system
+
 az login --identity
 
 az account set -s ${SUBSCRIPTION_ID}
 az aks get-credentials -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --overwrite-existing
 
-kubectl create ns istio-system
-kubectl --context="${CLUSTER_NAME}" create secret generic cacerts -n istio-system \
- --from-literal=ca-cert.pem=${CLUSTER_CA_CERT} \
- --from-literal=ca-key.pem=${CLUSTER_CA_KEY} \
- --from-literal=root-cert.pem=${CLUSTER_ROOT_CA_CERT} \
- --from-literal=cert-chain.pem=${CLUSTER_CERT_CHAIN} 
+kubectl create ns ${NAMESPACE}
+cat <<EOF | kubectl --context="${CLUSTER_NAME}" apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: cacerts
+  namespace: ${NAMESPACE}
+type: Opaque
+data:
+  ca-cert.pem: ${CLUSTER_CA_CERT}
+  ca-key.pem: ${CLUSTER_CA_KEY}
+  root-cert.pem: ${CLUSTER_ROOT_CA_CERT}
+  cert-chain.pem: ${CLUSTER_CERT_CHAIN}
+EOF
