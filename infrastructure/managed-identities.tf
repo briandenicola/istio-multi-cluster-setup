b296d70a5140dc9e00041f51ed8379227194d1ab
briandenicola <brian@briandenicola.com>
2022-06-02 15:10:35 -0500

    Restored identity

--- infrastructure/managed-identities.tf
+++ infrastructure/managed-identities.tf
@@ -9,10 +9,3 @@ resource "azurerm_user_assigned_identity" "aks_kubelet_identity" {
   resource_group_name = azurerm_resource_group.k8s.name
   location            = azurerm_resource_group.k8s.location
 }
-
-resource "azurerm_user_assigned_identity" "aks_service_mesh_identity" {
-  name                = "${var.cluster_name}-${var.service_mesh_type}-pod-identity"
-  resource_group_name = azurerm_resource_group.k8s.name
-  location            = azurerm_resource_group.k8s.location
-}
-
