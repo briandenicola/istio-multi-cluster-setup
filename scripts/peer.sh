79931236de00c7ab91c449db297c2c39f834c616
briandenicola <brian@briandenicola.com>
2022-09-16 17:30:33 -0500

    Bug fixes

--- scripts/peer.sh
+++ scripts/peer.sh
@@ -7,9 +7,13 @@ fi
 az network vnet peering create --name ${peer_name} \
     --remote-vnet ${SOUTH_CENTRAL_VNET_ID} \
     --resource-group ${CENTRAL_CLUSTER_RG} \
-    --vnet-name ${CENTRAL_VNET_NAME}
-
+    --vnet-name ${CENTRAL_VNET_NAME} \
+    --allow-vnet-access \
+    --allow-forwarded-traffic 
+    
 az network vnet peering create --name ${peer_name} \
     --remote-vnet ${CENTRAL_VNET_ID} \
     --resource-group ${SOUTH_CENTRAL_CLUSTER_RG} \
     --vnet-name ${SOUTH_CENTRAL_VNET_NAME}
+    --allow-vnet-access \
+    --allow-forwarded-traffic 
--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -3,11 +3,11 @@
 terraform -chdir=./infrastructure workspace select central
 export CENTRAL_CLUSTER_RG=$(terraform -chdir=./infrastructure output -raw CLUSTER_RG)
 export CENTRAL_CLUSTER_NAME=$(terraform -chdir=./infrastructure output -raw CLUSTER_NAME)
-export CENTRAL_VNET_NAME=$(terraform -chdir=./infrastructure output -raw VNET_RESOURCE_ID)
-export CENTRAL_VNET_ID=$(terraform -chdir=./infrastructure output -raw VNET_NAME)
+export CENTRAL_VNET_NAME=$(terraform -chdir=./infrastructure output -raw VNET_NAME)
+export CENTRAL_VNET_ID=$(terraform -chdir=./infrastructure output -raw VNET_RESOURCE_ID)
 
 terraform -chdir=./infrastructure workspace select southcentral
 export SOUTH_CENTRAL_CLUSTER_RG=$(terraform -chdir=./infrastructure output -raw CLUSTER_RG)
 export SOUTH_CENTRAL_CLUSTER_NAME=$(terraform -chdir=./infrastructure output -raw CLUSTER_NAME)
-export SOUTH_CENTRAL_VNET_NAME=$(terraform -chdir=./infrastructure output -raw VNET_RESOURCE_ID)
-export SOUTH_CENTRAL_VNET_ID=$(terraform -chdir=./infrastructure output -raw VNET_NAME)
+export SOUTH_CENTRAL_VNET_NAME=$(terraform -chdir=./infrastructure output -raw VNET_NAME)
+export SOUTH_CENTRAL_VNET_ID=$(terraform -chdir=./infrastructure output -raw VNET_RESOURCE_ID)
